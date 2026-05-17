16b580644229592c2da88fed3b5a04c4e11da9f2
rayanikolova <163350684+rayanikolova@users.noreply.github.com>
2026-05-17 19:30:08 +0300

    Added an Ingredient class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+class Ingredient {
+private:
+    string name;
+
+public:
+    Ingredient(string n) {
+        name = n;
+    }
+
+    string getName() {
+        return name;
+    }
+};
+
 int main() {
     cout << "Cookbook Project";
     return 0;
